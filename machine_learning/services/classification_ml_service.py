d2726e8da2cb285ddc521444033a75e63b05c012
GaylordCC <gcarricaballero3@hotmail.com>
2024-11-27 11:15:57 -0500

    Handling some data to perfomrt a first test classification

--- machine_learning/services/classification_ml_service.py
+++ machine_learning/services/classification_ml_service.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 
+import os
 
 class ClassificationAlgorithmService:
     def __init__(self) -> None:
@@ -14,4 +15,21 @@ class ClassificationAlgorithmService:
     ):
         mnist = fetch_openml('mnist_784', version=1)
         print(mnist.keys())
+
+        # Get mnist data and target
+        X, Y = mnist['data'], mnist['target']
+        print(X.shape)
+        print(Y.shape)
+        # 
+        digit = X.to_numpy()[0]
+        digit_image = digit.reshape(28,28)
+
+        results_graphics_path = 'results_graphics'
+        filenames = []
+        plt.imshow(digit_image, cmap='binary')
+        file_path = os.path.join(results_graphics_path, f"plot_classification.png")
+        plt.savefig(file_path)
+        plt.close()  # Cierra el gráfico para liberar memoria
+        filenames.append(file_path)
+
         return "ok"
