86f336e0e5798ff45f047ec5ebe0205f59389315
GaylordCC <gcarricaballero3@hotmail.com>
2024-11-27 10:54:31 -0500

    Add classification router

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from .routers import main_ml
+from .routers import main_ml, classification_ml
 
 # Create an instance of the FastAPI app
 app = FastAPI()
 
 # Include the router
 app.include_router(main_ml.router)
+app.include_router(classification_ml.router)
