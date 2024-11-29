be779069437a21a63fb77c946d24da194e00933e
GaylordCC <gcarricaballero3@hotmail.com>
2024-11-29 13:45:33 -0500

    Add sgd claassifier algorithm and training this one

--- machine_learning/services/classification_ml_service.py
+++ machine_learning/services/classification_ml_service.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
+from sklearn.linear_model import SGDClassifier
 
 import os
 
@@ -36,4 +37,13 @@ class ClassificationAlgorithmService:
         # Verifying the value teste above
         Y = Y.astype(np.uint8)
         print(Y[0])
+
+        X_train, X_test, Y_train, Y_test = X[:60000], X[60000:], Y[:60000], Y[60000:]
+        Y_train_5 = (Y_train == 5)
+        Y_test_5 = (Y_test == 5)
+
+        # SGDC Classifier (Algorithm)
+        sgd_classifier = SGDClassifier(random_state=42)
+        sgd_classifier.fit(X_train, Y_train_5)
+
         return "ok"
