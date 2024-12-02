2917f5ae8c75bfddc3df1f3952b56f289374edd3
GaylordCC <gcarricaballero3@hotmail.com>
2024-12-02 13:50:01 -0500

    Add confusion array, and some evaluation method

--- machine_learning/services/classification_ml_service.py
+++ machine_learning/services/classification_ml_service.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 
 import os
 
@@ -47,10 +48,25 @@ class ClassificationAlgorithmService:
         sgd_classifier = SGDClassifier(random_state=42)
         sgd_classifier.fit(X_train, Y_train_5)
 
-        # 
+        # corroborar con sgdc que el numero precido arriba si es el 5
         print(sgd_classifier.predict([digit]))
 
         # Medir el rendiemiento del modelo
         print(cross_val_score(sgd_classifier, X_train, Y_train_5, cv=3, scoring='accuracy'))
 
+        # Matriz de confusión:
+        # [
+        # TN"True-Negative": algotritmo dijo que no era 5, y estaba correcta, FP"False-Positive": algorimot dijo que eran 5, y se equivoco,
+        # FN"False-Negative": algoritmo dijo que no eran 5, y se equivoco, TP"True-Positive": algoritmo dijo que era 5, y estaba correcto
+        #]
+        Y_train_predict = cross_val_predict(sgd_classifier, X_train, Y_train_5, cv=3)
+        print(confusion_matrix(Y_train_5, Y_train_predict))
+
+        # Precision: TP/(TP+FP)
+        print(precision_score(Y_train_5, Y_train_predict))
+        # Memoria, recall: TP/(TP+FN)
+        print(recall_score(Y_train_5, Y_train_predict))
+
+        # F1: 
+        print(f1_score(Y_train_5, Y_train_predict))
         return "ok"
