97c6ebc58883f10e97863887bad509cd7f44243f
GaylordCC <gcarricaballero3@hotmail.com>
2024-12-02 18:12:13 -0500

    Add metho to implement the logic of the logistic regression classification algorithm

--- machine_learning/services/classification_ml_service.py
+++ machine_learning/services/classification_ml_service.py
@@ -1,11 +1,12 @@
 from fastapi import HTTPException
-
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_openml
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.preprocessing import OneHotEncoder
 
 import os
 
@@ -69,4 +70,52 @@ class ClassificationAlgorithmService:
 
         # F1: 
         print(f1_score(Y_train_5, Y_train_predict))
-        return "ok"
+        return "successfully image classification"
+    
+    def handle_logistic_classification(
+        self
+    ):
+        data = pd.read_csv('/home/gaylord/machine_learning_v01/machine_learning/sample_data/Social_Network_Ads.csv') # gecc laptop
+        
+        # Get data information
+        print(data.info())
+        # Get data description
+        print(data.describe())
+        # Retrieved column names
+        print(data.columns)
+
+        # Extract de column data
+        X = data.iloc[:, [2,3]]
+        Y = data.iloc[:, -1].values
+        print({"Y":Y, "X":X})
+
+        # Handle the categorical variable, and convert this one to numeric column
+        gender = data[['Gender']]
+        print({"Gender":gender})
+        cat_encoder = OneHotEncoder()
+        data_cat_1hot = cat_encoder.fit_transform(gender)
+        # Check how was the categories
+        print(cat_encoder.categories_)
+        # Check the conmversion process
+        print(data_cat_1hot.toarray()[:3])
+
+        # 
+        encoded_df = pd.DataFrame(data_cat_1hot.toarray(), columns = cat_encoder.get_feature_names_out())
+        print(encoded_df)
+        print('*'*50)
+
+        # Join variables
+        data1 = pd.concat([X, encoded_df], axis=1)
+        print(data1)
+        # Check the quantity of data
+        data1.shape
+        # Divide the data for training and testing
+        X_train, X_test, Y_train, Y_test = train_test_split(data1, Y, test_size=0.2, random_state=0)
+        print(X_train.shape)
+        print(X_test.shape)
+
+        # Scalate the data, to reduce the big difference between them
+
+
+        
+        return "successfully logistic classification"
