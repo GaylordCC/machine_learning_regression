b4487f0548ee07259fa360423784bf57611351b3
GaylordCC <gcarricaballero3@hotmail.com>
2025-03-05 19:56:13 -0500

    Update linear regression model

--- machine_learning/services/main_ml_service.py
+++ machine_learning/services/main_ml_service.py
@@ -65,7 +65,6 @@ class MachineLearningService:
             data = pd.read_csv('/mnt/c/Users/Gaylord Carrillo/Documents/develop/machine_learning_regression/machine_learning/sample_data/Advertising.csv') # gecc desktop 
             X = data[request.column_name].values.reshape(-1,1)   # Reshape 'TV', 'Radio', 'Newspaper' column values into a 2D array
             Y = data['Sales'].values
-
             # Split the data sample into trainind and testing data
             X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
             print(X_train.shape)
